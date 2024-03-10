8872c5f76e499a1cc19361c14d6be98f2457cd7d
brpo01 <opraise139@gmail.com>
2024-03-10 22:27:58 +0000

    wrote data migration script for tunstall store

---
+++ src/main/resources/db/migration/V1_2__InsertIntoTables.sql
@@ -0,0 +1,11 @@
+INSERT INTO pharmazeal.tunstall(date_of_birth, surname, middle_name, firstname, house_number, street_name, postcode, city, county, country)
+VALUES ('1989-02-11 00:00:00','John ','Jack','James',32,'Harvard Street','ST4 9AB','Stoke-on-Trent','Staffordshire ','United Kingdom'),
+                                  ('2001-01-19 00:00:00','Steve','Drew','Kao',12,'Bresford Street','ST4 8GB','Stoke-on-Trent','Staffordshire ','United Kingdom'),
+                                  ('1985-08-02 00:00:00','Lee','Mikel','Nu',10,'Stee Street','ST4 2ES','Stoke-on-Trent','Staffordshire ','United Kingdom'),
+                                  ('1995-11-20 00:00:00','Alee','Don','Ritz',88,'Combridge Street','ST4 2EU','Stoke-on-Trent','Staffordshire ','United Kingdom'),
+                                  ('1999-02-16 00:00:00','Rockey','Jong','Nu',101,'Food Street','ST4 2EN','Stoke-on-Trent','Staffordshire ','United Kingdom'),
+                                  ('2002-09-08 00:00:00','Shawn','Mendes','Firm',232,'Fenton Street','ST4 6RU','Stoke-on-Trent','Staffordshire ','United Kingdom'),
+                                  ('2000-11-13 00:00:00','Om','Barry','Sui',109,'College Streert','ST4 9ES','Stoke-on-Trent','Staffordshire ','United Kingdom'),
+                                  ('1988-03-17 00:00:00','Sarah','Veronica','Loo',290,'Canal Street','ST4 2LE','Stoke-on-Trent','Staffordshire ','United Kingdom'),
+                                  ('2002-01-03 00:00:00','Farah','Salah','You',133,'Park Streert','ST4 8WS','Stoke-on-Trent','Staffordshire ','United Kingdom'),
+                                  ('2007-12-12 00:00:00','Adam','Ceasar','Woi',99,'Central Street','ST4 2AU','Stoke-on-Trent','Staffordshire ','United Kingdom');
