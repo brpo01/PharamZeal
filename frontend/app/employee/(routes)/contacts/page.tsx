6f714f4563594aea67ea943ca44ec2e010d6f74a
kokoforbes <onikokoabdullahi@gmail.com>
2024-04-02 17:08:56 +0100

    updated contact page0

--- frontend/app/employee/(routes)/contacts/page.tsx
+++ frontend/app/employee/(routes)/contacts/page.tsx
@@ -7,24 +7,49 @@ import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 
-export default async function ContactsPage() {
+import { ContactColumn, columns } from "./components/columns";
+import { DataTable } from "@/components/ui/data-table";
+
+export default async function SalesPage() {
   const params = useParams();
   const router = useRouter();
+
+  const contacts: ContactColumn[] = [
+    {
+      store: "Tunstall",
+      phone: "+44 07-333-433-335",
+    },
+    {
+      store: "Fenton",
+      phone: "+44 07-333-433-336",
+    },
+    {
+      store: "Hanley",
+      phone: "+44 07-333-433-337",
+    },
+    {
+      store: "Longton",
+      phone: "+44 07-333-433-338",
+    },
+    {
+      store: "Stoke",
+      phone: "+44 07-333-433-339",
+    },
+  ];
 
   return (
     <div className='flex-col'>
       <div className='flex-1 space-y-4 p-8 pt-6'>
         <div className='flex items-center justify-between'>
-          <Heading
-            title={`Contacts`}
-            description='Manage drugs and medicines for your store'
-          />
+          <Heading title={`Contacts`} description='' />
           <Button onClick={() => router.push(`/medicine/new`)}>
             <Plus className='mr-2 h-4 w-4' /> Add New
           </Button>
         </div>
 
         <Separator />
+
+        <DataTable searchKey='store' columns={columns} data={contacts} />
       </div>
     </div>
   );
