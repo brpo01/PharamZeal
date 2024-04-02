de1d626d65c7d0eee4af6339954e1ba047f7eb44
kokoforbes <onikokoabdullahi@gmail.com>
2024-04-02 16:33:19 +0100

    Updated sales page

--- frontend/app/employee/(routes)/sales/page.tsx
+++ frontend/app/employee/(routes)/sales/page.tsx
@@ -7,9 +7,51 @@ import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 
+import { formatter } from "@/lib/utils";
+
+import { SaleColumn, columns } from "./components/columns";
+import { DataTable } from "@/components/ui/data-table";
+
 export default async function SalesPage() {
   const params = useParams();
   const router = useRouter();
+
+  const sales = [
+    {
+      id: "tututiti",
+      name: "praise bola",
+      drug: "paracetamol",
+      quantity: 2,
+      price: 5,
+      date_of_sale: "12/06/2024",
+    },
+    {
+      id: "tututitierr",
+      name: "gourav",
+      drug: "paracetamol",
+      quantity: 1,
+      price: 5,
+      date_of_sale: "12/06/2024",
+    },
+    {
+      id: "tututiti45",
+      name: "sayyed",
+      drug: "panadol",
+      quantity: 5,
+      price: 56,
+      date_of_sale: "12/07/2024",
+    },
+  ];
+
+  const formattedSales: SaleColumn[] = sales.map((item) => ({
+    id: item.id,
+    name: item.name,
+    drug: item.drug,
+    quantity: item.quantity,
+    date_of_sale: item.date_of_sale,
+    price: formatter.format(item.price),
+    // createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }));
 
   return (
     <div className='flex-col'>
@@ -25,6 +67,8 @@ export default async function SalesPage() {
         </div>
 
         <Separator />
+
+        <DataTable searchKey='name' columns={columns} data={formattedSales} />
       </div>
     </div>
   );
