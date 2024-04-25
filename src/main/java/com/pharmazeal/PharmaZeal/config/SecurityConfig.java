396164667d823330f0b5f1f8d0df29a1e4c73ff4
brpo01 <opraise139@gmail.com>
2024-04-25 15:03:46 +0100

    modified security config

--- src/main/java/com/pharmazeal/PharmaZeal/config/SecurityConfig.java
+++ src/main/java/com/pharmazeal/PharmaZeal/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
     private static final String[] WHITE_LIST_URL = {
             "/users/hello",
             "/users/login",
+            "/users/create"
     };
 
     @Bean
