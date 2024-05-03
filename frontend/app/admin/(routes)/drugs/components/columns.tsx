8db722aa5aa29f40d81df266daf76b99af07061e
kokoforbes <onikokoabdullahi@gmail.com>
2024-05-03 12:29:24 +0100

    Updated /admin/drugs page

--- frontend/app/admin/(routes)/drugs/components/columns.tsx
+++ frontend/app/admin/(routes)/drugs/components/columns.tsx
@@ -6,20 +6,36 @@ import { CellAction } from "./cell-action";
 
 export type DrugColumn = {
   id: string;
-  name: string;
-  quantity: number;
-  price: number;
+  price: string | number;
+  availability: boolean;
+  available_stock: number;
+  customer_condition: string;
+  drugName: string;
+  drug_code: string;
   expiry_date: string;
+  id_check: boolean;
+  idCheck?: boolean;
+  postcode: string;
+  sales: any;
+  store: string;
 };
 
 export const columns: ColumnDef<DrugColumn>[] = [
   {
-    accessorKey: "name",
-    header: "Name",
+    accessorKey: "drugName",
+    header: "Drug name",
   },
   {
-    accessorKey: "quantity",
-    header: "Quantity",
+    accessorKey: "store",
+    header: "Store",
+  },
+  {
+    accessorKey: "availability",
+    header: "Available",
+  },
+  {
+    accessorKey: "id_check",
+    header: "Check ID",
   },
   {
     accessorKey: "price",
